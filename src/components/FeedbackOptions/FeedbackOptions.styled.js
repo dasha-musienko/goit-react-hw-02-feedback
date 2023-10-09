7f53fbbfbc002102843794636f817b52745489cc
dasha-musienko <dasha04.musienko@gmail.com>
2023-10-09 14:53:14 +0300

    Adds features

---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const ButtonsWrapper = styled.ul`
+  display: flex;
+  align-items: center;
+  gap: 36px;
+`;
+
+export const Button = styled.button`
+  padding: 14px 30px;
+  background-color: transparent;
+  border-radius: 30px;
+  border: 2px solid ${({name}) => chooseColorByOption(name)};
+
+  font-size: 16px;
+  font-weight: 600;
+  color: #3E3E3E;
+
+  &:hover {
+    background-color: ${({name}) => chooseColorByOption(name)};
+    border-color: #3E3E3E;
+  }
+`;
+
+
+
+
+function chooseColorByOption (color) {
+    switch (color) {
+      case "good": 
+          return "#00D930";
+      case "neutral": 
+          return "#FFE661";
+       case "bad": 
+          return "#DD2800";
+        }
+  }
